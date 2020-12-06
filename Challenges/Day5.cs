57589bcc5bf0df55687bd9c2e7e45e5783bec7bc
Sebastian Łągiewski <226173@student.pwr.edu.pl>
2020-12-06 12:59:17 +0100

    Day5

---
+++ Challenges/Day5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2k20.Challenges
+{
+    public class Day5 : IAocChallenge
+    {
+        public string Evaluate()
+        {
+            var input = File.ReadAllLines("./input/Day5.txt");
+
+            return 
+                $"Part One: {PartOne(input)}" + Environment.NewLine +
+                $"Part Two: {PartTwo(input)}";
+        }
+
+
+        private int PartOne(string[] input) =>
+            input
+                .Select(GetSeatId)
+                .Max();
+
+        private int PartTwo(string[] input)
+        {
+            var seatIds = input
+                .Select(GetSeatId)
+                .ToList();
+
+            var allSeatIds = Enumerable.Range(0, 8)
+                .SelectMany(column =>
+                    Enumerable.Range(0, 128)
+                        .Select(row => GetSeatId(row, column))
+                )
+                .ToList();
+
+            var missingSeatIds = allSeatIds
+                .Except(seatIds)
+                .ToList();
+
+            return missingSeatIds.Single(missingSeatId =>
+                seatIds.Contains(missingSeatId + 1)
+                && seatIds.Contains(missingSeatId - 1)
+                );
+        }
+
+        private int GetSeatId(string seatCode) => GetSeatId(GetSeatRow(seatCode), GetSeatColumn(seatCode));
+
+        private int GetSeatId(int row, int column) => 8 * row + column;
+
+        private int GetSeatColumn(string seatCode) =>
+            Convert.ToInt32(
+                seatCode[^3..].Replace('L', '0').Replace('R', '1'),
+                2);
+
+        private int GetSeatRow(string seatCode) => 
+            Convert.ToInt32(
+                seatCode[..7].Replace('F', '0').Replace('B', '1'), 
+                2);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,17 +13,18 @@ namespace AoC2k20
                 { "Day1", new Challenges.Day1().Evaluate },
                 { "Day2", new Challenges.Day2().Evaluate },
                 { "Day3", new Challenges.Day3().Evaluate },
-                { "Day4", new Challenges.Day4().Evaluate }
+                { "Day4", new Challenges.Day4().Evaluate },
+                { "Day5", new Challenges.Day5().Evaluate },
             };
 
             foreach (var (challengeName, evaluate) in challenges)
             {
                 Console.WriteLine($"==== {challengeName} ====");
                 Console.WriteLine(evaluate());
+                Console.WriteLine(Environment.NewLine);
             }
 
             Console.ReadKey();
-            
         }
     }
 }
